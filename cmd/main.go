b62d8a1fc0ef566916064be64e4cacf4d7bd7b3e
Owain Lewis <owain@owainlewis.com>
2017-11-08 20:44:15 +0000

    First

---
+++ cmd/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/golang/glog"
+	"github.com/owainlewis/convoy/pkg/controller"
+	"k8s.io/client-go/kubernetes"
+)
+
+func main() {
+	// When running as a pod in-cluster, a kubeconfig is not needed. Instead this will make use of the service account injected into the pod.
+	// However, allow the use of a local kubeconfig as this can make local development & testing easier.
+	kubeconfig := flag.String("kubeconfig", "", "Path to a kubeconfig file")
+
+	// We log to stderr because glog will default to logging to a file.
+	// By setting this debugging is easier via `kubectl logs`
+	flag.Set("logtostderr", "true")
+	flag.Parse()
+
+	// Build the client config - optionally using a provided kubeconfig file.
+	config, err := common.GetClientConfig(*kubeconfig)
+	if err != nil {
+		glog.Fatalf("Failed to load client config: %v", err)
+	}
+
+	// Construct the Kubernetes client
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		glog.Fatalf("Failed to create kubernetes client: %v", err)
+	}
+
+	stopCh := make(chan struct{})
+	defer close(stopCh)
+
+	controller.NewConvoyController(client).Run(stopCh)
+}
