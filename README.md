00f3ed1ea8788f4f20fec21562600a959e9fcca4
Owain Lewis <owain@owainlewis.com>
2017-12-07 20:43:51 +0000

    Working rough impl

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Convoy
 
-A simple controller to notify you about Kubernetes cluster events
+Convoy is a Kubernetes controller that can notify you about specific cluster events. For example if you want to watch all 'Pod' events in a certain namespace then Convoy can dispatch these events to your team via a Slack channel
+
+## Configuration
+
+TODO
--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -1,11 +1,14 @@
 package controller
 
 import (
+	"fmt"
 	"log"
 	"time"
 
 	glog "github.com/golang/glog"
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	runtime "k8s.io/apimachinery/pkg/util/runtime"
 	wait "k8s.io/apimachinery/pkg/util/wait"
 	informercorev1 "k8s.io/client-go/informers/core/v1"
 	kubernetes "k8s.io/client-go/kubernetes"
@@ -13,6 +16,10 @@ import (
 	listerv1 "k8s.io/client-go/listers/core/v1"
 	cache "k8s.io/client-go/tools/cache"
 	workqueue "k8s.io/client-go/util/workqueue"
+)
+
+const (
+	ConvoyEventType = "Pod"
 )
 
 type ConvoyController struct {
@@ -36,12 +43,7 @@ func NewConvoyController(client kubernetes.Interface, informer informercorev1.Ev
 	informer.Informer().AddEventHandler(
 		cache.ResourceEventHandlerFuncs{
 			AddFunc: func(obj interface{}) {
-				event := obj.(*v1.Event)
-				glog.Infof("New event %v", event.Message)
-			},
-			UpdateFunc: func(oldObj, newObj interface{}) {
-				event := newObj.(*v1.Event)
-				glog.Infof("Update event %v", event.Message)
+				c.enqueue(obj)
 			},
 		},
 	)
@@ -49,23 +51,32 @@ func NewConvoyController(client kubernetes.Interface, informer informercorev1.Ev
 	return c
 }
 
+// Run will start the controller
 func (c *ConvoyController) Run(stopCh chan struct{}) {
-
 	defer c.queue.ShutDown()
-
-	glog.Info("Starting controller")
 
 	glog.Info("Waiting for cache sync")
 	if !cache.WaitForCacheSync(stopCh, c.eventListerSynced) {
 		glog.Info("Timeout waiting for caches to sync")
 		return
 	}
-	log.Print("caches are synced")
+	log.Print("Caches are synced")
 
 	go wait.Until(c.runWorker, time.Second, stopCh)
 
 	<-stopCh
 	glog.Info("Stopping controller")
+}
+
+func (c *ConvoyController) enqueue(obj interface{}) {
+	var key string
+	var err error
+
+	if key, err = cache.MetaNamespaceKeyFunc(obj); err != nil {
+		runtime.HandleError(err)
+		return
+	}
+	c.queue.AddRateLimited(key)
 }
 
 func (c *ConvoyController) runWorker() {
@@ -74,5 +85,84 @@ func (c *ConvoyController) runWorker() {
 }
 
 func (c *ConvoyController) processNextWorkItem() bool {
+	obj, shutdown := c.queue.Get()
+
+	if shutdown {
+		return false
+	}
+
+	// We wrap this block in a func so we can defer c.workqueue.Done.
+	err := func(obj interface{}) error {
+		// We call Done here so the workqueue knows we have finished
+		// processing this item. We also must remember to call Forget if we
+		// do not want this work item being re-queued. For example, we do
+		// not call Forget if a transient error occurs, instead the item is
+		// put back on the workqueue and attempted again after a back-off
+		// period.
+		defer c.queue.Done(obj)
+		var key string
+		var ok bool
+		// We expect strings to come off the workqueue. These are of the
+		// form namespace/name. We do this as the delayed nature of the
+		// workqueue means the items in the informer cache may actually be
+		// more up to date that when the item was initially put onto the
+		// workqueue.
+		if key, ok = obj.(string); !ok {
+			// As the item in the workqueue is actually invalid, we call
+			// Forget here else we'd go into a loop of attempting to
+			// process a work item that is invalid.
+			c.queue.Forget(obj)
+			runtime.HandleError(fmt.Errorf("expected string in workqueue but got %#v", obj))
+			return nil
+		}
+		// Run the syncHandler, passing it the namespace/name string of the
+		// Foo resource to be synced.
+		if err := c.syncHandler(key); err != nil {
+			return fmt.Errorf("error syncing '%s': %s", key, err.Error())
+		}
+		// Finally, if no error occurs we Forget this item so it does not
+		// get queued again until another change happens.
+		c.queue.Forget(obj)
+
+		return nil
+	}(obj)
+
+	if err != nil {
+		runtime.HandleError(err)
+		return true
+	}
+
 	return true
+}
+
+func (c *ConvoyController) syncHandler(key string) error {
+	// Convert the namespace/name string into a distinct namespace and name
+	namespace, name, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		runtime.HandleError(fmt.Errorf("invalid resource key: %s", key))
+		return nil
+	}
+
+	event, err := c.eventLister.Events(namespace).Get(name)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			runtime.HandleError(fmt.Errorf("foo '%s' in work queue no longer exists", key))
+			return nil
+		}
+
+		return err
+	}
+
+	c.processEvent(event)
+
+	return nil
+}
+
+func (c *ConvoyController) processEvent(event *v1.Event) {
+	if event.InvolvedObject.Kind == ConvoyEventType {
+		//now := meta_v1.Now()
+		//if !event.FirstTimestamp.Before(&now) {
+		glog.Infof("Processing %s event %s", event.InvolvedObject.Kind, event.Message)
+		//}
+	}
 }
